31d060e2fa32778c40731206b2b7e68e64bb573a
GabrielDSanta <eloviveiros@hotmail.com>
2022-08-26 23:17:06 -0300

    Aumento V2

--- script.js
+++ script.js
@@ -1,11 +1,49 @@
 
 var nome 
 var salario 
+var porcentagem 
 
 quest()
+
+console.log(`Nome: ${nome}, Salário Atual:  ${salario}`)
+
+
+informar()
+
+console.log(Calculator(salario, porcentagem))
+
+
+
 
 function quest(){
     nome = prompt("Qual o seu nome ?")
     salario = parseFloat(prompt("Qual o seu Salário Atual ?"))
 }
 
+function informar(){
+
+    if(salario <= 1500){
+        porcentagem = 1.20
+    }
+    
+    if(salario >= 1501){
+        porcentagem = 1.15
+    }
+    
+    if(salario >= 2001){
+        porcentagem = 1.10
+    }
+    
+    if(salario >= 3001){
+        porcentagem = 1.05
+    }
+
+    return porcentagem
+}
+
+function Calculator(salario){
+
+    console.log(`A Porcetagem é ${porcentagem}`)
+    return (`Parábens você teve um aumento ! Salario Atual: ${salario * porcentagem}`)
+   
+}
